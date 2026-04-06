f7fd45abf30f83d33da517703f6513c9f8feb58b
sohel160 <sohelcmc53rd@gmail.com>
2026-04-06 22:31:43 +0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,62 +2,64 @@ export default {
   async fetch(request) {
 
     const url = new URL(request.url)
+    const token = url.searchParams.get("token")
 
-    // token check
-    if (url.searchParams.get("token") !== "abc123") {
+    // token protection
+    if (token !== "abc123") {
       return new Response("Forbidden", { status: 403 })
     }
 
-    // allow only Clash clients
+    // detect client
     const ua = request.headers.get("User-Agent") || ""
+    const accept = request.headers.get("Accept") || ""
 
-    const allowedUA = [
+    // allow only Clash / FiClash clients
+    const allowedClients = [
       "Clash",
       "clash",
       "ClashMeta",
       "ClashforWindows",
       "ClashX",
-      "Stash"
+      "Stash",
+      "Shadowrocket",
+      "FiClash"
     ]
 
     let allowed = false
 
-    for (const a of allowedUA) {
-      if (ua.includes(a)) {
+    for (const client of allowedClients) {
+      if (ua.includes(client)) {
         allowed = true
         break
       }
     }
 
-    if (!allowed) {
+    // block browsers
+    if (!allowed && accept.includes("text/html")) {
       return new Response("404 Not Found", { status: 404 })
     }
 
+    // hidden proxy config
     const config = `
-proxy-providers:
-  myprovider:
-    type: http
-    url: "https://proxies-worker-gh.sohelcmc53rd.workers.dev/proxies?token=abc123"
-    interval: 3600
-    path: ./proxies.yaml
-    health-check:
-      enable: true
-      url: http://www.gstatic.com/generate_204
-      interval: 10
+proxies:
+  - name: "proxy1"
+    type: socks5
+    server: 45.115.113.114
+    port: 64182
 
-proxy-groups:
-- name: "FREE"
-  type: load-balance
-  strategy: round-robin
-  use:
-  - myprovider
-
-rules:
-- MATCH,FREE
+  - name: "proxy2"
+    type: socks5
+    server: 103.89.24.221
+    port: 65088
+    user: 1
+    pass: 1
 `
 
     return new Response(config, {
-      headers: { "Content-Type": "text/plain" }
+      headers: {
+        "Content-Type": "text/plain; charset=utf-8"
+      }
     })
+
   }
 }
